62ee88bc4dca1f6dfad36517f2bbfa9a39042d2a
Jerry Lee <lzjjerrywoodylee@gmail.com>
2017-12-02 23:35:47 +0900

    create index page

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const Koa = require('koa')
+const Router = require('koa-router')
+const koa_static = require('koa-static')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+const app = new Koa()
+const router = new Router()
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .use(koa_static(path.join(__dirname, 'public')))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+

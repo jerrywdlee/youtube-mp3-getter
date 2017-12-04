aa771f024d772f8e02e0c2b188df4a293a3a086b
Jerry Lee <lzjjerrywoodylee@gmail.com>
2017-12-04 09:28:00 +0900

    isolate getVideoId method

--- puppeteers/p_02.js
+++ puppeteers/p_02.js
@@ -3,6 +3,7 @@
 
 'use strict'
 const puppeteer = require('puppeteer')
+const getVideoId = require('../utils/get_video_id')
 
 let getMp3Urls = async (youtubeUrl) => {
 
@@ -26,21 +27,6 @@ let getMp3Urls = async (youtubeUrl) => {
   mp3Urls.push(mp3Url)
   await browser.close()
   return mp3Urls
-}
-
-let getVideoId = (youtubeUrl) => {
-  // youtubeUrl likes https://www.youtube.com/watch?v=i62Zjga8JOM
-  // OR like https://youtu.be/30Ef7i3qq-U
-  let videoId = ''
-  let [subId_1, subId_2] = youtubeUrl.trim().split('watch?v=')
-  videoId = subId_2 ? subId_2 : subId_1 // if not url just a video ID, will work
-  if (videoId.indexOf('youtu.be\/') > -1) {
-    // if like https://youtu.be/30Ef7i3qq-U , will work
-    [subId_1, subId_2] = videoId.split('youtu.be\/')
-    videoId = subId_2 ? subId_2 : subId_1
-  }
-  if (!videoId) return null
-  return videoId
 }
 
 module.exports = getMp3Urls;
